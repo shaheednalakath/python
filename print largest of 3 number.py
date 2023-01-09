e3181c52cf062163c0392a8a354635ebca7bc68d
MUHAMMED ABDUL SHAHEED.N <116936159+shaheednalakath@users.noreply.github.com>
2023-01-09 14:04:17 +0530

    Add files via upload

---
+++ print largest of 3 number.py
@@ -0,0 +1,9 @@
+a=int(input("Enter 1 number"))
+b=int(input("Enter 2 number"))
+c=int(input("Enter 3 number"))
+if a>b:
+    print("first no is greater")
+elif c>b:
+    print("Third no is greater")
+else:
+    print("Second no is greater")
