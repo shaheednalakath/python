0e6df671cc714ba0b68190091d30bd67c5fbe8b0
MUHAMMED ABDUL SHAHEED.N <116936159+shaheednalakath@users.noreply.github.com>
2023-01-09 13:39:48 +0530

    printnumber pattern

---
+++ co2_5.py
@@ -0,0 +1,16 @@
+n=int(input("enter the step number"))
+for i in  range(1,n+1):
+    for j in range(1,i+1):
+        print(i*j,end=" ")
+    print('\n')
+
+
+
+# enter the step number4
+# 1
+#
+# 2 4
+#
+# 3 6 9
+#
+# 4 8 12 16
