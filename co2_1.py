703ce0aa03e3c1448e5c485a9903f49ce4637a03
MUHAMMED ABDUL SHAHEED.N <116936159+shaheednalakath@users.noreply.github.com>
2023-01-09 13:33:44 +0530

    fact of a number

---
+++ co2_1.py
@@ -0,0 +1,15 @@
+n=int(input("enter a number"))
+fact=1
+if n==0 or n==1:
+    print("1")
+else:
+    for i in range(1,n+1):
+        fact=fact*i
+    print(fact)
+
+
+
+
+
+# enter a number5
+# 120
